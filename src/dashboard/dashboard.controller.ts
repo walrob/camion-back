4f16da49fb9c6ad86d94140237e5f98aafc9d95d
walgrobledo <walgrobledo@gmail.com>
2026-06-28 10:46:02 -0300

    Fase 8 - Dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { Role } from 'src/common/enums/role.enum';
-import { ActiveUser } from 'src/common/decorators/active-user.decorator';
-import { ActiveUserInterface } from 'src/common/interfaces/active-user.interface';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Dashboard')
@@ -12,9 +10,9 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get()
-  @Auth(Role.ADMIN)
-  getDashboard(@ActiveUser() user: ActiveUserInterface) {
-    return this.dashboardService.getDashboard(user);
+  @Get('overview')
+  @Auth(Role.ADMIN, Role.MANAGER, Role.DISPATCHER)
+  getOverview() {
+    return this.dashboardService.getOverview();
   }
 }
